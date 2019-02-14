74005d8a56066cffe9058778e4f6b42d6639aafd
bakatrouble <bakatrouble@gmail.com>
2019-02-14 19:47:48 +0300

    add license

--- src/_tgvoip.cpp
+++ src/_tgvoip.cpp
@@ -1,3 +1,24 @@
+/*
+ * PytgVoIP - Telegram VoIP Library for Python
+ * Copyright (C) 2019 bakatrouble <https://github.com/bakatrouble>
+ *
+ * This file is part of PytgVoIP.
+ *
+ * PytgVoIP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PytgVoIP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PytgVoIP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "_tgvoip.h"
 #include <iostream>
 // #include <chrono>
