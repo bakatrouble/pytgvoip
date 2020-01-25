ead6f52e726da5e626a33308a4dbd0a82e98e5cd
bakatrouble <bakatrouble@gmail.com>
2020-01-26 01:10:09 +0300

    fix

--- ci_scripts/01-openssl.bat
+++ ci_scripts/01-openssl.bat
@@ -6,11 +6,11 @@ if "%PYTHON_ARCH%"=="32" (
     set COMPILER=VC-WIN64A
 )
 
-if exist "openssl/Release/lib/ssleay32.lib" goto ALREADY_BUILT
+if exist "openssl_1_1_1/Release/lib/ssleay32.lib" goto ALREADY_BUILT
 
 echo Building OpenSSL...
-git clone https://github.com/openssl/openssl.git
-cd openssl
+git clone https://github.com/openssl/openssl.git openssl_1_1_1
+cd openssl_1_1_1
 git checkout OpenSSL_1_1_1-stable
 
 perl Configure no-shared "%COMPILER%"
