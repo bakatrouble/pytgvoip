88cd6164b32304e765f4fec1dbf1640551eb9d52
bakatrouble <bakatrouble@gmail.com>
2020-01-26 01:05:04 +0300

    update openssl

--- ci_scripts/01-openssl.bat
+++ ci_scripts/01-openssl.bat
@@ -2,10 +2,8 @@ cd ..
 
 if "%PYTHON_ARCH%"=="32" (
     set COMPILER=VC-WIN32
-    set DO_MS_SCRIPT=ms\do_ms.bat
 ) else (
     set COMPILER=VC-WIN64A
-    set DO_MS_SCRIPT=ms\do_win64a.bat
 )
 
 if exist "openssl/Release/lib/ssleay32.lib" goto ALREADY_BUILT
@@ -13,13 +11,16 @@ if exist "openssl/Release/lib/ssleay32.lib" goto ALREADY_BUILT
 echo Building OpenSSL...
 git clone https://github.com/openssl/openssl.git
 cd openssl
-git checkout OpenSSL_1_0_1-stable
-perl Configure no-shared --prefix=Release --openssldir=%PREFIX% "%COMPILER%"
-call %DO_MS_SCRIPT%
-nmake -f ms\nt.mak
-nmake -f ms\nt.mak install
-xcopy tmp32\lib.pdb Release\lib\
-nmake -f ms\nt.mak clean
+git checkout OpenSSL_1_1_1-stable
+
+perl Configure no-shared "%COMPILER%"
+nmake
+mkdir "out%PYTHON_ARCH%"
+move libcrypto.lib "out%PYTHON_ARCH%"
+move libssl.lib "out%PYTHON_ARCH%"
+move ossl_static.pdb "out%PYTHON_ARCH%"
+nmake clean
+
 cd ..
 goto FINISH
 
