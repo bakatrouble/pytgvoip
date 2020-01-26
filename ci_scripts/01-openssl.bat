a3079c8823391ed528afb1dec115c94df7c34c3d
bakatrouble <bakatrouble@gmail.com>
2020-01-26 04:14:22 +0300

    fix

--- ci_scripts/01-openssl.bat
+++ ci_scripts/01-openssl.bat
@@ -9,6 +9,14 @@ if "%PYTHON_ARCH%"=="32" (
 if exist "openssl_1_1_1" goto ALREADY_BUILT
 
 echo Building OpenSSL...
+
+curl -L -o nasminst.exe http://libgd.blob.core.windows.net/nasm/nasm-2.07-installer.exe
+start /wait nasminst.exe /S
+set PATH=C:\Program Files (x86)\nasm;%PATH%
+
+choco install strawberryperl -y
+set PATH=C:\strawberry\c\bin;C:\strawberry\perl\site\bin;C:\strawberry\perl\bin;%PATH%
+
 git clone https://github.com/openssl/openssl.git openssl_1_1_1
 cd openssl_1_1_1
 git checkout OpenSSL_1_1_1-stable
