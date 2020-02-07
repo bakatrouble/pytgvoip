0497e734a1c3bf874a264274b397af265797989e
bakatrouble <bakatrouble@gmail.com>
2020-02-07 18:21:07 +0300

    fix build script

--- ci_scripts/03-build.bat
+++ ci_scripts/03-build.bat
@@ -1,4 +1,4 @@
-xcopy /E /I /C ..\openssl_1_1_1\out64 ..\openssl_1_1_1\out32
-xcopy /E /I /C ..\opus\win32\VS2015\x64 ..\opus\win32\VS2015\Win32
+xcopy /E /I /C /Y ..\openssl_1_1_1\out64 ..\openssl_1_1_1\out32
+xcopy /E /I /C /Y ..\opus\win32\VS2015\x64 ..\opus\win32\VS2015\Win32
 python setup.py build
 python setup.py bdist_wheel
